6d66ffbc62c5db5efe88f05686ae109f32ee460c
zhou-yuhan <zhouyuhan_@outlook.com>
2023-12-29 14:26:44 +0800

    dev: add zhuge package

--- rebuild-pkg.sh
+++ rebuild-pkg.sh
@@ -1,6 +1,6 @@
 pkg_name=$1
 
-make package/${pkg_name}/compile V=s
+make package/${pkg_name}/{clean,prepare,compile} V=s
 
 pkg_bin=$(ls bin/packages/aarch64_cortex-a53/rt_streaming/ | grep $pkg_name)
 
